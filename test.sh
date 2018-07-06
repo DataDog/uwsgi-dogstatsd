836ca4f3bfb372847c5e5fafb918cadc987f13ef
Willem D'Haeseleer <dhwillem@gmail.com>
2018-07-05 20:40:27 -0700

    build from source and wait for uwsgi

--- test.sh
+++ test.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 cd example && ./run.sh &
+
+until $(curl --output /dev/null -k --silent --fail http://localhost:9090/); do
+    echo 'Waiting for uwsgi...'
+    sleep 1
+done
+
 python tests/test.py
 exit $?
